fb40ae36ae584db3ff4dedab69c6d886177a9cb8
Jessicanayara <jjessyka.nay@gmail.com>
2023-09-20 20:42:53 -0300

    Initial commit

---
+++ src/DesafioContador/Contador.java
@@ -0,0 +1,38 @@
+package DesafioContador;
+
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            //chamando o método contendo a lógica de contagem
+            contar(parametroUm, parametroDois);
+
+        }catch (ParametrosInvalidosException exception) {
+           System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+    }
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if(parametroUm>parametroDois){
+            throw new ParametrosInvalidosException();
+        }else{
+            int contagem = parametroDois - parametroUm;
+            int num=0;
+            for(var i=0;i<contagem;i++) {
+                num+=1;
+                System.out.println("imprimindo valor "+ num);
+
+
+            }
+        }
+
+    }
+}
